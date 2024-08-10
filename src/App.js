35d0c251bc7fe01276c53d8e17c2a431f8774a40
YosefSameh <139604190+YosefSameh@users.noreply.github.com>
2024-08-10 08:05:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 
 import Rigster from './Components/Rigster/rigster';
 import Login from './Components/Login/login';
-import { Route, Routes } from 'react-router-dom';
+import {  Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import Nav from './Components/Nav/nav';
 import Home from "./Components/Home/home";
 import ProfilePage from './Components/Profile/profil';
 import Save from './Components/Saved/save';
 import { ToastContainer } from 'react-toastify';
+import { useEffect } from 'react';
 
 function App() {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!token) {
+        
+        if (location.pathname !== '/' && location.pathname !== '/login') {
+            navigate('/');
+        }
+    } else {
+
+        if (location.pathname === '/' || location.pathname === '/login') {
+            navigate('/home');
+        }
+    }
+}, [token, location.pathname, navigate]);
 
   return (
-    <div className="App">
+
+      <div className="App">
+
       <Nav/>
 
       <ToastContainer
@@ -29,7 +49,8 @@ theme="light"
 
 />
 
-  <Routes>
+
+ <Routes>
     <Route path="/" element={<Rigster/>} /> 
     <Route path="home" element={<Home/>} /> 
     <Route path="login" element={<Login/>} /> 
@@ -37,11 +58,12 @@ theme="light"
     <Route path="profile/:userId" element={<ProfilePage/>} /> 
     <Route path="profile" element={<ProfilePage/>} /> 
     <Route path="saved" element={<Save/>} /> 
-    </Routes>
+    </Routes> 
 
     
         
     </div>
+  
   );
 }
 
